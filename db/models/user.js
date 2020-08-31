b9499943dbd2182f51137794245e8deb86f20082
djicegiggles <shaieleigh1@gmail.com>
2020-08-31 16:11:22 -0400

    model changes 0831Mon

--- db/models/user.js
+++ db/models/user.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         type: DataTypes.STRING,
         validates: {
-          len: [1, 255],
+          len: [4, 40],
         },
       },
       hashedPassword: {
