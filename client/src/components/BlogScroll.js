10ba9325a658067cfe92439a2cb9c81d2c8e5e60
djicegiggles <shaieleigh1@gmail.com>
2020-11-06 20:19:39 -0500

    blog scroll ;

--- client/src/components/BlogScroll.js
+++ client/src/components/BlogScroll.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import TextIcon from './TextIcon.js';
+
 export default function BlogScroll() {
   return (
     <div className='blogScroll'>
       <div className='profPicDBScroll'>
         <img src='https://64.media.tumblr.com/5a4406dbfeeca31d14dfb9f41a25d800/72d826b0c13960da-15/s64x64u_c1/f5f285077fe67a397c3e406778b7e7deffdb8c06.pnj' />
+      </div>
+      <div>
+        <div>
+          <span class="material-icons">text_format</span>
+        </div>
+        <div>
+          <TextIcon />
+        </div>
       </div>
     </div>
   )
