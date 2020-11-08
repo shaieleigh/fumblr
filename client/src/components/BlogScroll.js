586cec2c5908505ec23e0d1ceb4a03481ed740cb
djicegiggles <shaieleigh1@gmail.com>
2020-11-08 11:31:36 -0500

    blog scroll

--- client/src/components/BlogScroll.js
+++ client/src/components/BlogScroll.js
@@ -8,15 +8,21 @@ export default function BlogScroll() {
       <div className='profPicDBScroll'>
         <img src='https://64.media.tumblr.com/5a4406dbfeeca31d14dfb9f41a25d800/72d826b0c13960da-15/s64x64u_c1/f5f285077fe67a397c3e406778b7e7deffdb8c06.pnj' />
       </div>
-      <div>
-        <div>
-          <span className="material-icons">text_format</span>
+      <main className='dashboardMain'>
+        <div className='dashboardNavBar'>
+          {/* <div>
+            <span className="material-icons">text_format</span>
+          </div> */}
+          <div className='dashNavButtons'><TextIcon /><p className='dashbIconFont'>Text</p></div>
+          <div className='dashNavButtons'><TextIcon /><p className='dashbIconFont'>Text</p></div>
+          <div className='dashNavButtons'><TextIcon /><p className='dashbIconFont'>Text</p></div>
+          <div className='dashNavButtons'><TextIcon /><p className='dashbIconFont'>Text</p></div>
+          <div className='dashNavButtons'><TextIcon /><p className='dashbIconFont'>Text</p></div>
+          <div className='dashNavButtons'><TextIcon /><p className='dashbIconFont'>Text</p></div>
+          <div className='dashNavButtons'><TextIcon /><p className='dashbIconFont'>Text</p></div>
+          <div><i className="fas fa-headphones-alt"></i></div>
         </div>
-        <div>
-          <TextIcon />
-        </div>
-        <div><i className="fas fa-headphones-alt"></i></div>
-      </div>
+      </main>
     </div>
   )
 }
